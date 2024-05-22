8c0ce0c92839870eab149c6430309b01e8727769
Aamir Mukaram <aamirmukaram@gmail.com>
2024-05-22 22:45:51 +0200

    Fixed pagination

--- src/app/features/commits/commits.component.ts
+++ src/app/features/commits/commits.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, ViewChild, AfterViewInit } from '@angular/core';
 import { CommitsService, SearchCommit, SearchCommitsRequest } from './commits.service';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule, PageEvent, MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-commits',
@@ -15,30 +15,26 @@ import { MatTableDataSource } from '@angular/material/table';
 export class CommitsComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   private commitsService = inject(CommitsService);
+  private activatedRoute = inject(ActivatedRoute);
   displayedColumns: string[] = ['name', 'url', 'message'];
-  dataSource = new MatTableDataSource<SearchCommit>();
+  dataSource: SearchCommit[] = []; 
   totalCount = 0;
   pageSize = 30;
-
-  constructor() {
-
-  }
+  private urlParams = this.activatedRoute.snapshot.params;
 
   fetchTableData(request: SearchCommitsRequest) {
     this.commitsService.searchCommits(request)
       .subscribe(commits => {
-        this.dataSource.data = commits.items;
         this.totalCount = commits.totalCount;
+        this.dataSource = commits.items;
       });
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-
     this.fetchTableData({
       searchTerm: 'angular',
-      owner: 'angular',
-      repo: 'angular',
+      owner: this.urlParams['owner'],
+      repo: this.urlParams['repo'],
       page: 1,
       perPage: this.pageSize
     });
@@ -47,8 +43,8 @@ export class CommitsComponent implements AfterViewInit {
   pageChanged(event: PageEvent) {
     this.fetchTableData({
       searchTerm: 'angular',
-      owner: 'angular',
-      repo: 'angular',
+      owner: this.urlParams['owner'],
+      repo: this.urlParams['repo'],
       page: event.pageIndex + 1,
       perPage: event.pageSize
     });
